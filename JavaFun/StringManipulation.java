88d7e2fbf4424c72c52736c048032a908fa292b2
RomellPineda <romell.pineda@gmail.com>
2018-08-02 21:51:47 -0700

    modified StringManipulation.java

--- JavaFun/StringManipulation.java
+++ JavaFun/StringManipulation.java
@@ -1,4 +1,6 @@
 public class StringManipulation {
+    
+    // Trim both input strings and then concatenate them.
     public String trimAndConcat(String a, String b) {
         String atrim = a.trim();
         String btrim = b.trim();
@@ -6,6 +8,7 @@ public class StringManipulation {
         return processed;
     }
 
+    // Get the index of the character.
     public Integer getIndexOrNull(String a, char b) {
         Integer yield = a.indexOf(b);
         if(yield == -1){
@@ -14,6 +17,7 @@ public class StringManipulation {
         return yield;
     }
 
+    // Get the index of the start of the substring.
     public Integer getIndexOrNull(String a, String b) {
         Integer yield = a.indexOf(b);
         if(yield == -1){
@@ -22,6 +26,7 @@ public class StringManipulation {
         return yield;
     }
 
+    // Get a substring using a starting and ending index, and concatenate that with the second string input to our method.
     public String concatSubstring(String a, int b, int c, String d){
         String spliced = a.substring(b,c);
         String mod = spliced.concat(d);
