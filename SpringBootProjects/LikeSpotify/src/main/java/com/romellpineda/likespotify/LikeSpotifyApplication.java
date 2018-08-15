0a6e3d9dd8f8fa2d28131f91033d018997aa0b40
RomellPineda <romell.pineda@gmail.com>
2018-08-14 23:25:17 -0700

    added LikeSpotify

---
+++ SpringBootProjects/LikeSpotify/src/main/java/com/romellpineda/likespotify/LikeSpotifyApplication.java
@@ -0,0 +1,12 @@
+package com.romellpineda.likespotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeSpotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeSpotifyApplication.class, args);
+	}
+}
